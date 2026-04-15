29653e020444c1af7af2ce3c4513bdfa8ec6590c
TheSemiColin <146498846+TheSemiColin@users.noreply.github.com>
2026-04-15 01:15:49 -0500

    Metrics

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,23 @@ FROM python:3.12-slim
 # Set working directory
 WORKDIR /app
 
-# Copy model and source code
-COPY ./models /app/models
-COPY app.py .
+# Only run apt-get if INSTALL_BUILD_TOOLS is true
+RUN if [ "$INSTALL_BUILD_TOOLS" = "true" ] ; then \
+    apt-get update && apt-get install -y \
+    gcc \
+    python3-dev \
+    && rm -rf /var/lib/apt/lists/* ; \
+    fi
+
 COPY requirements.txt .
 
 # Install dependencies from requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-ENV PRECISION=f32
+# Copy model and source code
+COPY ./models /app/models
+COPY app.py .
+
 ENV ARCH=amd64
 # Make container accept command line arguments
 ENTRYPOINT ["python", "app.py"]
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import sys
 import logging
 import time
+import psutil
 
 # 1. Silence Hugging Face and OpenMP warnings
 os.environ["TRANSFORMERS_VERBOSITY"] = "error"
@@ -10,22 +11,22 @@ os.environ["KMP_WARNINGS"] = "0"
 logging.getLogger("transformers").setLevel(logging.ERROR)
 logging.getLogger("optimum").setLevel(logging.ERROR)
 
+
 from transformers import T5Tokenizer
 from optimum.onnxruntime import ORTModelForSeq2SeqLM
+from sentence_transformers import SentenceTransformer, util
 
+embedder_path = "./models/embedding_model"
+embedder = SentenceTransformer(embedder_path)
 
-precision = os.getenv("PRECISION", "f32").lower()
 arch = os.getenv("ARCH", "amd64").lower()
 
-if precision == "f32":
-    MODEL_DIR = "./models/t5-small-onnx"
+if arch == "amd64":
+    MODEL_DIRS = ["./models/t5-small-onnx", "./models/t5-small-onnx-quantized-amd64"]
 else:
-    MODEL_DIR = f"./models/t5-small-onnx-quantized-{arch}"
+    MODEL_DIRS = ["./models/t5-small-onnx", "./models./t5-smalll-onnx-quantized-arm64"]
 
-print(f"🚀 Loading {precision.upper()} model optimized for {arch.upper()} from: {MODEL_DIR}")
-
-tokenizer = T5Tokenizer.from_pretrained(MODEL_DIR, legacy=False)
-model = ORTModelForSeq2SeqLM.from_pretrained(MODEL_DIR, provider="CPUExecutionProvider")
+print(f"🚀 Performing Benchmark comparison for {arch.upper()} models in F32 and then INT8")
 
 if len(sys.argv) < 2:
     print("Usage: python app.py \"<text to summarize>\"")
@@ -34,23 +35,48 @@ if len(sys.argv) < 2:
 text = " ".join(sys.argv[1:])
 
 input_text = "summarize: " + text
+summaries = []
+embeddings = []
+process = psutil.Process(os.getpid())
+process.cpu_percent(interval=None)
 
-inputs = tokenizer(input_text, return_tensors="pt")
 
-start = time.time()
-summary_ids = model.generate(
-    inputs["input_ids"],
-    max_length=60,
-    min_length=30,
-    num_beams=4,
-    length_penalty=2.0,
-    early_stopping=True,
-    use_cache=True
-)
-end = time.time()
+for MODEL_DIR in MODEL_DIRS:
+    if MODEL_DIR.endswith("quantized-amd64") or MODEL_DIR.endswith("quantized-arm64"):
+        print(f"\n📊 Benchmarking quantized model (INT8)")
+    else:
+        print(f"\n📊 Benchmarking full precision model (F32)")
 
-summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    tokenizer = T5Tokenizer.from_pretrained(MODEL_DIR, legacy=False)
+    model = ORTModelForSeq2SeqLM.from_pretrained(MODEL_DIR, provider="CPUExecutionProvider")
+    inputs = tokenizer(input_text, return_tensors="pt")
 
-print("Summary: ", summary)
+    process.cpu_percent(interval=None)
 
-print(f"Latency: {end - start:.2f}s")
+    start = time.time()
+    summary_ids = model.generate(
+        inputs["input_ids"],
+        max_length=60,
+        min_length=30,
+        num_beams=4,
+        length_penalty=2.0,
+        early_stopping=True,
+        use_cache=True
+    )
+    end = time.time()
+    usage = process.cpu_percent(interval=None)
+    per_core = psutil.cpu_percent(interval=None, percpu=True)
+    
+    print(f"CPU Usage: {usage}%")
+    print(f"Per-core CPU Usage: {per_core}")
+    print(f"Latency: {end - start:.2f}s")
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    print("Summary: ", summary)
+    summaries.append(summary)
+
+for summary in summaries:
+    embeddings.append(embedder.encode(summary, convert_to_tensor=True))
+
+cos_sim = util.cos_sim(embeddings[0], embeddings[1])
+
+print(f"\n🔍 Cosine Similarity between summaries: {cos_sim.item():.4f}")
